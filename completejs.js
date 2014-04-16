6e54c53ff127856b8dcc911fc63e62f885558dbb
Leonardo Donelli <learts92@gmail.com>
2014-04-16 23:46:05 +0200

    Add option for case sensitivity, code cleanup.

--- completejs.js
+++ completejs.js
@@ -1,22 +1,35 @@
 function Complete(el, data) {
+
+	// Config properties
+	this.caseSensitive = false;
+	this.exact = true;
+
+	// DOM elements
 	var list = document.createElement('ul');
 	var dropDown = document.createElement('div');
 	dropDown.appendChild(list);
 	document.body.appendChild(dropDown);
-	el.addEventListener('input', test);
 
-	
+	// Listeners
+	el.addEventListener('input', lookup);
 
+	// I'm wasteful and stupid. Please improve me.
+	function lookup(e) {
 
-	function test(e) {
+		// delete all the previous matches
 		while (list.firstChild) {
 				list.firstChild.remove();
 		}
 
-		console.log(el.value);
-
+		// add the new ones
 		data.forEach(function(d) {
-			if (d.startsWith(el.value)) {
+			var candidate = d;
+			var toMatch = el.value;
+			if (!this.caseSensitive) {
+				candidate = candidate.toLowerCase();
+				toMatch = toMatch.toLowerCase();
+			}
+			if (candidate.startsWith(toMatch)) {
 				var optionValue = document.createTextNode(d);
 				var newOption = document.createElement('li');
 				newOption.appendChild(optionValue);
