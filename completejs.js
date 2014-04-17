dadaeb40225f70f7e44f0b3e6d9aaef715592bbd
Leonardo Donelli <learts92@gmail.com>
2014-04-17 02:09:35 +0200

    Code refactoring (fuck Javascript scoping!), fixes case sensitivity setting not really working.

--- completejs.js
+++ completejs.js
@@ -4,37 +4,44 @@ function Complete(el, data) {
 	this.caseSensitive = false;
 	this.exact = true;
 
+	// Data
+	this.data = data;
+
 	// DOM elements
-	var list = document.createElement('ul');
-	var dropDown = document.createElement('div');
-	dropDown.appendChild(list);
-	document.body.appendChild(dropDown);
+	this.inputElement = el;
+	this.list = document.createElement('ul');
+	this.dropDown = document.createElement('div');
+	this.dropDown.appendChild(this.list);
+	document.body.appendChild(this.dropDown);
 
 	// Listeners
-	el.addEventListener('input', lookup);
+	var selfie = this; // just bear with me
+	el.addEventListener('input', function cb(e) { selfie.lookup(); });
 
-	// I'm wasteful and stupid. Please improve me.
-	function lookup(e) {
-
-		// delete all the previous matches
-		while (list.firstChild) {
-				list.firstChild.remove();
-		}
-
-		// add the new ones
-		data.forEach(function(d) {
-			var candidate = d;
-			var toMatch = el.value;
-			if (!this.caseSensitive) {
-				candidate = candidate.toLowerCase();
-				toMatch = toMatch.toLowerCase();
-			}
-			if (candidate.startsWith(toMatch)) {
-				var optionValue = document.createTextNode(d);
-				var newOption = document.createElement('li');
-				newOption.appendChild(optionValue);
-				list.appendChild(newOption);
-			}
-		});
-	}
 }
+
+// I'm wasteful and stupid. Please improve me.
+Complete.prototype.lookup = function() {
+
+	// delete all the previous matches
+	while (this.list.firstChild) {
+		this.list.firstChild.remove();
+	}
+
+	// add the new ones
+	var selfie = this;
+	this.data.forEach(function(d) {
+		var candidate = d;
+		var toMatch = selfie.inputElement.value;
+		if (!selfie.caseSensitive) {
+			candidate = candidate.toLowerCase();
+			toMatch = toMatch.toLowerCase();
+		}
+		if (candidate.startsWith(toMatch)) {
+			var optionValue = document.createTextNode(d);
+			var newOption = document.createElement('li');
+			newOption.appendChild(optionValue);
+			selfie.list.appendChild(newOption);
+		}
+	});
+};
